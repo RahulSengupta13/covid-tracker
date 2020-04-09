f3e60aa197c9694672429bd09bfe10612745251e
Rahul Sengupta <rahul.sengupta@match.com>
2020-04-09 16:45:14 -0500

    CT-3 minor cleanup

--- home/src/main/java/com/rahulsengupta/home/di/modules/HomeViewModelModule.kt
+++ home/src/main/java/com/rahulsengupta/home/di/modules/HomeViewModelModule.kt
@@ -3,13 +3,14 @@ package com.rahulsengupta.home.di.modules
 import androidx.lifecycle.ViewModel
 import com.rahulsengupta.core.di.ViewModelKey
 import com.rahulsengupta.home.HomeActivityViewModel
+import com.rahulsengupta.network.di.DataSourceModule
 import com.rahulsengupta.network.di.NetworkModule
 import com.rahulsengupta.persistence.di.PersistenceModule
 import dagger.Binds
 import dagger.Module
 import dagger.multibindings.IntoMap
 
-@Module(includes = [NetworkModule::class, PersistenceModule::class])
+@Module(includes = [DataSourceModule::class, PersistenceModule::class])
 abstract class HomeViewModelModule {
 
     @Binds

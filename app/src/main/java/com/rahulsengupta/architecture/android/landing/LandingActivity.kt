1b3020a7ba3527b0d41bedf2a6007034a3862144
Rahul Sengupta <rahul.sengupta@match.com>
2020-04-11 15:37:44 -0500

    CT-3 hides botton navigation on non-top level destinations

--- app/src/main/java/com/rahulsengupta/architecture/android/landing/LandingActivity.kt
+++ app/src/main/java/com/rahulsengupta/architecture/android/landing/LandingActivity.kt
@@ -1,18 +1,30 @@
 package com.rahulsengupta.architecture.android.landing
 
 import android.os.Bundle
-import androidx.core.view.GravityCompat
+import android.view.View
 import androidx.databinding.DataBindingUtil
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.NavigationUI
 import com.rahulsengupta.architecture.R
 import com.rahulsengupta.architecture.databinding.ActivityLandingBinding
 import com.rahulsengupta.core.base.InjectableActivity
+import kotlinx.android.synthetic.main.activity_landing.*
 
 class LandingActivity : InjectableActivity() {
 
+    companion object {
+        private val TOP_LEVEL_DESTINATIONS = setOf(
+            R.id.dashboard,
+            R.id.settings,
+            R.id.live_reports
+        )
+    }
+
     private lateinit var navController: NavController
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    private var currentNavId = R.id.splashFragment
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,7 +36,17 @@ class LandingActivity : InjectableActivity() {
         binding.lifecycleOwner = this
 
         navController = findNavController(R.id.nav_fragment)
+        appBarConfiguration = AppBarConfiguration(TOP_LEVEL_DESTINATIONS)
         NavigationUI.setupWithNavController(binding.bottomNavigationView, navController)
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            currentNavId = destination.id
+            val isTopLevelDestination = TOP_LEVEL_DESTINATIONS.contains(destination.id)
+            bottomNavigationView.visibility = if (isTopLevelDestination) {
+                View.VISIBLE
+            } else {
+                View.GONE
+            }
+        }
     }
 
     override fun onSupportNavigateUp() = navController.navigateUp() || super.onSupportNavigateUp()

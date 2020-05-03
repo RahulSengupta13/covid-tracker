2bcbc89f17e91134f38e1bdb7ea21a98cafda083
Rahul Sengupta <rahul.sengupta@match.com>
2020-05-02 20:51:52 -0500

    set theme on startup

--- app/src/main/java/com/rahulsengupta/architecture/android/flows/settings/SettingsFragment.kt
+++ app/src/main/java/com/rahulsengupta/architecture/android/flows/settings/SettingsFragment.kt
@@ -1,21 +1,19 @@
 package com.rahulsengupta.architecture.android.flows.settings
 
 import android.content.SharedPreferences
-import android.os.Build
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatDelegate
-import androidx.lifecycle.ViewModelProvider
 import androidx.preference.ListPreference
 import androidx.preference.PreferenceFragmentCompat
 import androidx.preference.PreferenceManager
 import com.rahulsengupta.architecture.R
 import com.rahulsengupta.core.di.Injectable
+import com.rahulsengupta.core.usecase.ThemeHelperUseCase
 import javax.inject.Inject
 
 class SettingsFragment : PreferenceFragmentCompat(), Injectable {
 
     @Inject
-    lateinit var viewModelFactory: ViewModelProvider.Factory
+    lateinit var themeHelper: ThemeHelperUseCase
 
     lateinit var preference: SharedPreferences
 
@@ -24,25 +22,10 @@ class SettingsFragment : PreferenceFragmentCompat(), Injectable {
         preference = PreferenceManager.getDefaultSharedPreferences(requireContext())
 
         val preference = findPreference<ListPreference>("theme")
-        preference?.setOnPreferenceChangeListener { preference, newValue ->
+        preference?.setOnPreferenceChangeListener { _, newValue ->
             val themeValues = resources.getStringArray(R.array.theme_values)
             val value = (newValue as? String)
-            when (themeValues.indexOf(value)) {
-                0 -> {
-                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
-                }
-                1 -> {
-                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
-                }
-                2 -> {
-                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
-                        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM)
-                    } else {
-                        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_AUTO_BATTERY)
-                    }
-                }
-                else -> Unit
-            }
+            themeHelper.setTheme(themeValues, value)
             true
         }
 

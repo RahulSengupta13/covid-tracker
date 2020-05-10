1966735990faca430713fb7f7f10a06fd6482350
Rahul Sengupta <rahul.sengupta@match.com>
2020-05-10 13:06:00 -0500

    adds search adapter

--- app/src/main/java/com/rahulsengupta/architecture/android/flows/search/SearchToolbar.kt
+++ app/src/main/java/com/rahulsengupta/architecture/android/flows/search/SearchToolbar.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.util.AttributeSet
 import android.view.LayoutInflater
 import android.view.View
+import android.widget.ArrayAdapter
 import androidx.appcompat.widget.Toolbar
 import androidx.core.widget.doOnTextChanged
 import com.rahulsengupta.architecture.R
@@ -36,7 +37,10 @@ class SearchToolbar @JvmOverloads constructor(
             context.hideKeyboard(search_close_icon)
         }
 
+        val suggestions: List<String> = listOf("united states", "united kingdom", "united india")
+        val adapter = ArrayAdapter(context, android.R.layout.simple_list_item_1, suggestions)
+        search_title.setAdapter(adapter)
+        search_title.dropDownVerticalOffset =
+            resources.getDimensionPixelOffset(R.dimen.standard_half_margin)
     }
-
-
 }
